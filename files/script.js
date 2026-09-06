a838ff1a098ddf7d91d01566b1b63a8f2bbf73d7
Alyssa Elias <aly080809@gmail.com>
2026-09-06 20:44:38 +0000

    commit to main branch

--- files/script.js
+++ files/script.js
@@ -3,19 +3,20 @@ dragElement(document.getElementById("window")).element.onmousedown = startDraggi
 
 
 function dragElement(element) {
-    var initialX = 0;
+  var initialX = 0;
   var initialY = 0;
   var currentX = 0;
   var currentY = 0;
   e = e || window.event;
-    e.preventDefault();
-    currentX = initialX - e.clientX;
-    currentY = initialY - e.clientY;
-    initialX = e.clientX;
-    initialY = e.clientY;
-    element.style.top = (element.offsetTop - currentY) + "px";
-    element.style.left = (element.offsetLeft - currentX) + "px";
+  e.preventDefault();
+  currentX = initialX - e.clientX;
+  currentY = initialY - e.clientY;
+  initialX = e.clientX;
+  initialY = e.clientY;
+  element.style.top = (element.offsetTop - currentY) + "px";
+  element.style.left = (element.offsetLeft - currentX) + "px";
 }
+
 
   
 
@@ -53,3 +54,34 @@ var welcomeScreenOpen = document.querySelector("#welcomeopen")
 welcomeScreenOpen.addEventListener("click", function() {
   openWindow(welcomeScreen);
 });
+
+var selectedIcon = undefined
+
+function selectIcon(element) {
+  element.classList.add("selected");
+  selectedIcon = element
+} 
+
+function deselectIcon(element) {
+  element.classList.remove("selected");
+  selectedIcon = undefined
+}
+
+function handleIconTap(element) {
+  if (element.classList.contains("selected")) {
+    deselectIcon(element)
+    openWindow(window)
+  } else {
+    selectIcon(element)
+  }
+}
+
+dragElement(document.querySelector("#notes"))
+
+var welcomeScreen = document.querySelector("#welcome")
+var notesScreenClose = document.querySelector("#welcomeclose")
+notesScreenClose.addEventListener("click", () => closeWindow(welcomeScreen));
+
+var notesScreen = document.querySelector("#notes")
+var notesScreenClose = document.querySelector("#notesclose")
+notesScreenClose.addEventListener("click", () => closeWindow(notesScreen));
